b40233872e4aab68c0f391c58ac9cc5cbd04d01c
Johan Andersson <joha@netcompany.com>
2018-03-21 23:12:36 +0100

    Add indexer

--- src/main/groovy/org/gls/LangServer.groovy
+++ src/main/groovy/org/gls/LangServer.groovy
@@ -1,21 +1,24 @@
 package org.gls
 
+
 import groovy.transform.TypeChecked
+import groovy.util.logging.Slf4j
 import java.util.concurrent.CompletableFuture
 import org.eclipse.lsp4j.InitializeParams
 import org.eclipse.lsp4j.InitializeParams
 import org.eclipse.lsp4j.InitializeResult;
+import org.eclipse.lsp4j.MessageParams;
+import org.eclipse.lsp4j.MessageType;
+import org.eclipse.lsp4j.ServerCapabilities
+import org.eclipse.lsp4j.jsonrpc.Launcher
+import org.eclipse.lsp4j.launch.LSPLauncher
+import org.eclipse.lsp4j.services.LanguageClient
+import org.eclipse.lsp4j.services.LanguageClient
+import org.eclipse.lsp4j.services.LanguageClientAware
 import org.eclipse.lsp4j.services.LanguageServer
 import org.eclipse.lsp4j.services.TextDocumentService
 import org.eclipse.lsp4j.services.WorkspaceService
-import org.eclipse.lsp4j.services.LanguageClient
-import org.eclipse.lsp4j.launch.LSPLauncher
-import org.eclipse.lsp4j.jsonrpc.Launcher
-import org.eclipse.lsp4j.services.LanguageClientAware
-import org.eclipse.lsp4j.services.LanguageClient
-import org.eclipse.lsp4j.ServerCapabilities
-
-import groovy.util.logging.Slf4j
+import org.gls.groovy.GroovyIndexer
 
 @Slf4j
 @TypeChecked
@@ -24,6 +27,7 @@ class LangServer implements LanguageServer, LanguageClientAware {
     private LanguageClient client
     private WorkspaceService workspaceService
     private TextDocumentService textDocumentService
+    private GroovyIndexer indexer
 
     public LangServer() {
         this.workspaceService = new GroovyWorkspaceService()
@@ -32,8 +36,19 @@ class LangServer implements LanguageServer, LanguageClientAware {
 
     @Override
     CompletableFuture<InitializeResult> initialize(InitializeParams initializeParams) {
+        log.info "initialize: ${initializeParams}"
+        client.showMessage(new MessageParams(MessageType.Info, "Initializing langserver capabilities..."))
+
+        String rootUri = initializeParams.getRootUri()
+        log.info "rootUri: " + rootUri
+        indexer = new GroovyIndexer(rootUri: rootUri)
+        indexer.startIndexing()
+
         ServerCapabilities capabilities = new ServerCapabilities()
         return CompletableFuture.completedFuture(new InitializeResult(capabilities))
+    }
+
+    def startIndexing() {
     }
 
     @Override
@@ -43,6 +58,7 @@ class LangServer implements LanguageServer, LanguageClientAware {
 
     @Override
     void exit(){
+        log.info "Stopping langserver"
     }
 
     @Override
---
+++ src/main/groovy/org/gls/groovy/GroovyIndexer.groovy
@@ -0,0 +1,14 @@
+package org.gls.groovy
+import groovy.util.logging.Slf4j
+
+@Slf4j
+class GroovyIndexer {
+
+    String rootUri
+
+    def startIndexing() {
+        log.info "Indexing..."
+
+    }
+
+}
