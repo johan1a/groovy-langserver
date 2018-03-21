91d7395048e0784150bf3bb46401cf78284d2428
Johan Andersson <joha@netcompany.com>
2018-03-21 23:29:17 +0100

    traverse base dir and look for groovy files

--- src/main/groovy/org/gls/groovy/GroovyIndexer.groovy
+++ src/main/groovy/org/gls/groovy/GroovyIndexer.groovy
@@ -1,14 +1,27 @@
 package org.gls.groovy
+
 import groovy.util.logging.Slf4j
+import groovy.transform.TypeChecked
 
 @Slf4j
+@TypeChecked
 class GroovyIndexer {
 
-    String rootUri
+  String rootUri
 
     def startIndexing() {
+      try {
         log.info "Indexing..."
-
+          File basedir = new File(new URL(rootUri).toURI())
+          log.info "baseDir: ${basedir}"
+          basedir.eachFileRecurse {
+            if (it.name =~ /.*\.groovy/) {
+              log.info it.name
+            }
+          }
+      } catch(Exception e) {
+        log.error("error", e)
+      }
     }
 
 }
