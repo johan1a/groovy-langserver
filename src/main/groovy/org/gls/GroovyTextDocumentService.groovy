ff010c6ab1374328b7d1098710662be101e940f3
Johan Andersson <joha@netcompany.com>
2018-04-02 19:11:41 +0200

    refactor

--- src/main/groovy/org/gls/GroovyTextDocumentService.groovy
+++ src/main/groovy/org/gls/GroovyTextDocumentService.groovy
@@ -6,17 +6,94 @@ import org.eclipse.lsp4j.*
 import org.eclipse.lsp4j.jsonrpc.messages.Either
 import org.eclipse.lsp4j.services.TextDocumentService
 import org.gls.lang.ReferenceFinder
-
+import org.eclipse.lsp4j.services.LanguageClientAware
 import java.util.concurrent.CompletableFuture
+import org.gls.groovy.GroovyIndexer
+import org.codehaus.groovy.control.messages.*
+import org.eclipse.lsp4j.services.LanguageClient
+import org.codehaus.groovy.control.CompilationUnit
+import org.codehaus.groovy.control.ErrorCollector
+import org.codehaus.groovy.syntax.SyntaxException
+import org.eclipse.lsp4j.*
 
 @Slf4j
 @TypeChecked
-class GroovyTextDocumentService implements TextDocumentService {
+class GroovyTextDocumentService implements TextDocumentService, LanguageClientAware {
 
     private ReferenceFinder finder
+    private List<File> changedFiles = new LinkedList<>()
+    private LanguageClient client
+
+    GroovyIndexer indexer
 
     void setReferenceStorage(ReferenceFinder finder) {
         this.finder = finder
+    }
+
+    void setIndexer(GroovyIndexer indexer) {
+        this.indexer = indexer
+        indexer.index()
+        sendDiagnostics(indexer.getErrorCollector(), client)
+    }
+
+    private void showClientMessage(String message) {
+        client?.showMessage(new MessageParams(MessageType.Info, message))
+    }
+
+    private void sendDiagnostics(ErrorCollector errorCollector, LanguageClient client) {
+        log.info "Logging errors..."
+        log.info("errorCollector: ${errorCollector}")
+        try {
+            if(errorCollector == null) {
+                return
+            }
+            List<SyntaxErrorMessage> errors = errorCollector.getErrors()
+            List<Message> warnings = errorCollector.getWarnings()
+            log.info("errors: ${errors}")
+            log.info("warnings: ${warnings}")
+            Map<String, List<Diagnostic> > diagnosticMap = new HashMap<>()
+            errors?.each {
+                SyntaxException exception = it.getCause()
+                String uri = "file://" + exception.getSourceLocator()
+                Diagnostic diagnostic = asDiagnostic(exception)
+
+                List<Diagnostic> diagnostics = diagnosticMap.get(uri)
+                if(diagnostics == null) {
+                    diagnostics = new LinkedList<>()
+                    diagnosticMap.put(uri, diagnostics)
+                }
+                diagnostics.add(diagnostic)
+            }
+            warnings?.each {
+                log.info it.toString()
+                log.info "TODO implement warning diagnostics"
+            }
+            diagnosticMap.keySet().each {
+                PublishDiagnosticsParams params = new PublishDiagnosticsParams(it, diagnosticMap.get(it))
+                log.info("PARAMS: ${params}")
+                log.info("client?: ${client}")
+                client?.publishDiagnostics(params)
+            }
+        } catch (Exception e) {
+            log.error("Error", e)
+        }
+    }
+
+    private Diagnostic asDiagnostic(SyntaxException exception) {
+        log.info "${exception.getMessage()}"
+        int line = exception.getLine() - 1
+        Position start = new Position(line, exception.getStartColumn())
+        Position end = new Position(line, exception.getEndColumn())
+        Range range = new Range(start, end)
+
+        Diagnostic diagnostic = new Diagnostic(range, exception.getMessage())
+        return diagnostic
+    }
+
+    @Override
+    void connect(LanguageClient client) {
+        log.info "Connected to client."
+        this.client = client
     }
 
     @Override
@@ -123,6 +200,7 @@ class GroovyTextDocumentService implements TextDocumentService {
 
     @Override
     public void didChange(DidChangeTextDocumentParams params) {
+        changedFiles.add(new File(params.textDocument.uri))
         log.info "didChange"
     }
 
@@ -133,7 +211,7 @@ class GroovyTextDocumentService implements TextDocumentService {
 
     @Override
     public void didSave(DidSaveTextDocumentParams params) {
-        log.info "didSave"
+        log.info "didSave ${params}"
     }
 
 }
--- src/main/groovy/org/gls/LangServer.groovy
+++ src/main/groovy/org/gls/LangServer.groovy
@@ -14,33 +14,28 @@ import org.eclipse.lsp4j.MessageType;
 import org.eclipse.lsp4j.ServerCapabilities
 import org.eclipse.lsp4j.jsonrpc.Launcher
 import org.eclipse.lsp4j.launch.LSPLauncher
-import org.eclipse.lsp4j.services.LanguageClient
-import org.eclipse.lsp4j.services.LanguageClientAware
 import org.eclipse.lsp4j.services.LanguageServer
 import org.eclipse.lsp4j.services.TextDocumentService
 import org.eclipse.lsp4j.services.WorkspaceService
-import org.gls.groovy.GroovyIndexer
 import org.gls.lang.ReferenceStorage
-import org.codehaus.groovy.control.messages.*
 import org.codehaus.groovy.control.CompilationUnit
 import org.codehaus.groovy.control.ErrorCollector
 import org.codehaus.groovy.syntax.SyntaxException
+import org.eclipse.lsp4j.services.LanguageClient
 import org.eclipse.lsp4j.*
+import org.gls.groovy.GroovyIndexer
 
 
 @Slf4j
 @TypeChecked
-class LangServer implements LanguageServer, LanguageClientAware {
+class LangServer implements LanguageServer {
 
     public static final String DEFAULT_SRC_DIR = "/src/main/groovy"
-    private LanguageClient client
     private WorkspaceService workspaceService
     private GroovyTextDocumentService textDocumentService
 
-    GroovyIndexer indexer
-
     GroovyIndexer getIndexer() {
-        return indexer
+        return textDocumentService.indexer
     }
 
     LangServer() {
@@ -51,74 +46,18 @@ class LangServer implements LanguageServer, LanguageClientAware {
     @Override
     CompletableFuture<InitializeResult> initialize(InitializeParams initializeParams) {
         log.info "initialize: ${initializeParams}"
-        showClientMessage("Initializing langserver capabilities...")
+        textDocumentService.showClientMessage("Initializing langserver capabilities...")
 
         URI rootUri = new URI(initializeParams.getRootUri() + DEFAULT_SRC_DIR)
         log.info "rootUri: " + rootUri
         ReferenceFinder finder = new ReferenceFinder()
-        textDocumentService.setReferenceStorage(finder)
-        indexer = new GroovyIndexer(rootUri, finder)
+        GroovyIndexer indexer = new GroovyIndexer(rootUri, finder)
 
-        indexer.index()
-        sendDiagnostics(indexer.getErrorCollector(), client)
+        textDocumentService.setReferenceStorage(finder)
+        textDocumentService.setIndexer(indexer)
 
         ServerCapabilities capabilities = new ServerCapabilities()
         return CompletableFuture.completedFuture(new InitializeResult(capabilities))
-    }
-
-    private void sendDiagnostics(ErrorCollector errorCollector, LanguageClient client) {
-        log.info "Logging errors..."
-        log.info("errorCollector: ${errorCollector}")
-        try {
-            if(errorCollector == null) {
-                return
-            }
-            List<SyntaxErrorMessage> errors = errorCollector.getErrors()
-            List<Message> warnings = errorCollector.getWarnings()
-            log.info("errors: ${errors}")
-            log.info("warnings: ${warnings}")
-            Map<String, List<Diagnostic> > diagnosticMap = new HashMap<>()
-            errors?.each {
-                SyntaxException exception = it.getCause()
-                String uri = "file://" + exception.getSourceLocator()
-                Diagnostic diagnostic = asDiagnostic(exception)
-
-                List<Diagnostic> diagnostics = diagnosticMap.get(uri)
-                if(diagnostics == null) {
-                    diagnostics = new LinkedList<>()
-                    diagnosticMap.put(uri, diagnostics)
-                }
-                diagnostics.add(diagnostic)
-            }
-            warnings?.each {
-                log.info it.toString()
-                log.info "TODO implement warning diagnostics"
-            }
-            diagnosticMap.keySet().each {
-                PublishDiagnosticsParams params = new PublishDiagnosticsParams(it, diagnosticMap.get(it))
-                log.info("PARAMS: ${params}")
-                log.info("client?: ${client}")
-                client?.publishDiagnostics(params)
-            }
-        } catch (Exception e) {
-            log.error("Error", e)
-        }
-    }
-
-    private Diagnostic asDiagnostic(SyntaxException exception) {
-        log.info "${exception.getMessage()}"
-        int line = exception.getLine() - 1
-        Position start = new Position(line, exception.getStartColumn())
-        Position end = new Position(line, exception.getEndColumn())
-        Range range = new Range(start, end)
-
-        Diagnostic diagnostic = new Diagnostic(range, exception.getMessage())
-        return diagnostic
-    }
-
-
-    private void showClientMessage(String message) {
-        client?.showMessage(new MessageParams(MessageType.Info, message))
     }
 
     @Override
@@ -137,23 +76,17 @@ class LangServer implements LanguageServer, LanguageClientAware {
     }
 
     @Override
-    TextDocumentService getTextDocumentService() {
+    GroovyTextDocumentService getTextDocumentService() {
         return textDocumentService
-    }
-
-    @Override
-    void connect(LanguageClient client) {
-        log.info "Connected to client."
-        this.client = client
     }
 
     static void main(String[] args) {
         log.info "Starting langserver"
-        LanguageServer server = new LangServer()
+        LangServer server = new LangServer()
         Launcher<LanguageClient> launcher = LSPLauncher.createServerLauncher(server, System.in,
                     System.out)
         LanguageClient client = launcher.getRemoteProxy()
-        server.connect(client)
+        server.textDocumentService.connect(client)
         launcher.startListening()
     }
 }
