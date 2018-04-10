73784606aa80ce94530d92d261b70f8e25136438
Johan Andersson <joha@netcompany.com>
2018-04-10 07:44:17 +0200

    refactor

--- src/main/groovy/org/gls/lang/ReferenceMatcher.groovy
+++ src/main/groovy/org/gls/lang/ReferenceMatcher.groovy
@@ -1,14 +1,12 @@
 package org.gls.lang
 
+import org.eclipse.lsp4j.Position
 import org.eclipse.lsp4j.ReferenceParams
 import org.eclipse.lsp4j.TextDocumentPositionParams
 
-/**
- * Created by johan on 4/9/18.
- */
 class ReferenceMatcher<R extends Reference, D extends Definition> {
 
-    List<ImmutableLocation> getReferences(Set<D> definitions, Set<R> allUsages, ReferenceParams params) {
+    static List<ImmutableLocation> getReferences(Set<D> definitions, Set<R> allUsages, ReferenceParams params) {
         Optional<D> definitionOptional = findMatchingDefinition(definitions, params)
         definitionOptional.map { definition ->
             Set<R> usages = definition.findMatchingReferences(allUsages)
@@ -16,7 +14,7 @@ class ReferenceMatcher<R extends Reference, D extends Definition> {
         }.orElse([])
     }
 
-    List<ImmutableLocation> getDefinition(Set<D> definitions, Set<R> references, TextDocumentPositionParams params) {
+    static List<ImmutableLocation> getDefinition(Set<D> definitions, Set<R> references, TextDocumentPositionParams params) {
         Optional<R> usageOptional = findMatchingReference(references, params)
         usageOptional.map { matchingUsage ->
             Optional<D> definition = matchingUsage.findMatchingDefinition(definitions)
@@ -28,22 +26,22 @@ class ReferenceMatcher<R extends Reference, D extends Definition> {
 
     static Optional<R> findMatchingReference(Set<R> references, TextDocumentPositionParams params) {
         return Optional.ofNullable(references.find {
-            it.getSourceFileURI() == params.textDocument.uri &&
-                    it.columnNumber <= params.position.character &&
-                    it.lastColumnNumber >= params.position.character &&
-                    it.lineNumber <= params.position.line &&
-                    it.lastLineNumber >= params.position.line
+            mathesPosition(it, params.textDocument.uri, params.position)
         })
     }
 
     static Optional<D> findMatchingDefinition(Set<D> definitions, ReferenceParams params) {
         return Optional.ofNullable(definitions.find {
-            it.getSourceFileURI() == params.textDocument.uri &&
-                    it.columnNumber <= params.position.character &&
-                    it.lastColumnNumber >= params.position.character &&
-                    it.lineNumber <= params.position.line &&
-                    it.lastLineNumber >= params.position.line
+            mathesPosition(it, params.textDocument.uri, params.position)
         })
+    }
+
+    static boolean mathesPosition(HasLocation hasLocation, String uri, Position position) {
+        hasLocation.getSourceFileURI() == uri &&
+                hasLocation.columnNumber <= position.character &&
+                hasLocation.lastColumnNumber >= position.character &&
+                hasLocation.lineNumber <= position.line &&
+                hasLocation.lastLineNumber >= position.line
     }
 
 }
