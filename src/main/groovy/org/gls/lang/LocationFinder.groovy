209fcb7a8d5c87235e375465a97b0901a9e861a3
Johan Andersson <joha@netcompany.com>
2018-05-24 19:48:08 +0200

    use short name for location finding

--- src/main/groovy/org/gls/lang/LocationFinder.groovy
+++ src/main/groovy/org/gls/lang/LocationFinder.groovy
@@ -15,18 +15,19 @@ class LocationFinder {
         return findLocation(sourceFilePath, source, node, name, node.getAnnotations().size())
     }
 
-    static ImmutableLocation findLocation(String sourceFilePath, List<String> source, ASTNode node, String name, int lineOffset = 0) {
+    static ImmutableLocation findLocation(String sourceFilePath, List<String> source, ASTNode node, String fullName, int lineOffset = 0) {
         int lineNumber = node.lineNumber - 1 + lineOffset
         int lastLineNumber = lineNumber
         int columnNumber
         int lastColumnNumber
-        if (lineNumber > 0) {
+        if (lineNumber >= 0) {
             String firstLine = source[lineNumber]
-            if(firstLine != null && name != null && firstLine.contains(name)){
-                columnNumber = firstLine.indexOf(name, node.columnNumber - 1)
-                lastColumnNumber = columnNumber + name.size() - 1
+            String shortName = fullName.split("\\.").last()
+            if (firstLine != null && shortName != null && firstLine.contains(shortName)) {
+                columnNumber = firstLine.indexOf(shortName, node.columnNumber - 1)
+                lastColumnNumber = columnNumber + shortName.size() - 1
             } else {
-                log.warn("line doesn't contain name: $name, line: $firstLine")
+                log.warn("line doesn't contain name: ${shortName}, line: $firstLine")
                 columnNumber = node.columnNumber
                 lastColumnNumber = node.lastColumnNumber
             }
---
+++ src/test/groovy/org/gls/lang/LocationFinderSpec.groovy
@@ -0,0 +1,25 @@
+package org.gls.lang
+
+import org.codehaus.groovy.ast.ASTNode
+import spock.lang.Specification
+
+class LocationFinderSpec extends Specification {
+
+    void "Test findLocation"() {
+        given:
+            String sourceFilePath = "x"
+            List<String> source = ["def varName = new ClassName()"]
+            String fullName = "pkg.test.ClassName"
+            ASTNode node = new ASTNode(lineNumber: 1)
+
+        when:
+            ImmutableLocation location = LocationFinder.findLocation(sourceFilePath, source, node, fullName)
+
+        then:
+            location.range.start.line == 0
+            location.range.end.line == 0
+            location.range.start.character == 18
+            location.range.end.character == 26
+
+    }
+}
